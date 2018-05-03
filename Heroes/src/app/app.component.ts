5bcd920b35f64f9014c259a1950e01c0e9d30bea
Tanya Zamotaylo <zamotaylo.t@gmail.com>
2018-05-03 17:45:41 +0300

    add new file and style

--- Heroes/src/app/app.component.ts
+++ Heroes/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<label>Введите имя:</label>
-                 <input [(ngModel)]="name" placeholder="name">
-                 <h1>Добро пожаловать {{name}}!</h1>`
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
     name= '';
