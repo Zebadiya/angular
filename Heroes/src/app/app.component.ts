7d8005aa8d3d96ed0cc2a93a53bcb43e16b528de
Tatiana Zamotailo <zamotailo.t@gmail.com>
2018-06-02 13:26:00 +0300

    add service

--- Heroes/src/app/app.component.ts
+++ Heroes/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {DataService} from './data.service';
+import {Phone} from './phone';
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    providers: [DataService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     name= '';
+
+    condition: boolean=true;
+    toggle(){
+        this.condition=!this.condition;
+    }
+
+    items: Phone[] = [];
+    constructor(private dataService: DataService){}
+
+    addItem(name: string, price: number){
+
+      this.dataService.addData(name, price);
+    }
+    ngOnInit(){
+      this.items= this.dataService.getData();
+    }
 }
